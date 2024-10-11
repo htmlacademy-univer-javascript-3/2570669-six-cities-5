3ecbd0907bc7c97252a648c473a2cab5f018b6c7
Ksenia <uniks04@yandex.ru>
2024-10-11 14:19:16 +0500

    fixed

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
-import {MainScreen} from './pages/MainScreen'
+import {MainScreen} from './pages/MainScreen';
 
 type AppScreenProps = {
   placesCount: number;
 }
 function App({placesCount}: AppScreenProps){
   return (
-      <MainScreen placesCount={placesCount}/>
+    <MainScreen placesCount={placesCount}/>
   );
-};
+}
 export default App;
