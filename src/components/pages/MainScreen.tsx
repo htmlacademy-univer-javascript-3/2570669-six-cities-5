3f0343e7fc3a112fa71c98c69e4d30ea8dd22019
uniks <uniks04@yandex.ru>
2024-10-15 15:42:57 +0500

    3 task

--- src/components/pages/MainScreen.tsx
+++ src/components/pages/MainScreen.tsx
@@ -1,23 +1,22 @@
 import Card from './Card';
+import Logo from '../Logo';
+import { Helmet } from 'react-helmet-async';
+
 type MainScreenProps = {
     placesCount: number;
 }
-export function MainScreen({placesCount}: MainScreenProps){
+
+function MainScreen({placesCount}: MainScreenProps){
   return (
     <div className="page page--gray page--main">
+      <Helmet>
+        <title>6 Городов</title>
+      </Helmet>
       <header className="header">
         <div className="container">
           <div className="header__wrapper">
             <div className="header__left">
-              <a className="header__logo-link header__logo-link--active">
-                <img
-                  className="header__logo"
-                  src="img/logo.svg"
-                  alt="6 cities logo"
-                  width={81}
-                  height={41}
-                />
-              </a>
+              <Logo/>
             </div>
             <nav className="header__nav">
               <ul className="header__nav-list">
