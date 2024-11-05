2cac1965d98fd6104dc0313ade175bc18fe6622c
uniks <uniks04@yandex.ru>
2024-11-05 22:37:36 +0500

    я не понимаю...

--- src/components/offersList.tsx
+++ src/components/offersList.tsx
@@ -3,11 +3,12 @@ import { OffersType } from '../types/types';
 
 interface OffersListProps {
   offers: OffersType[];
+  listType: string;
 }
 
-function OffersList({ offers }: OffersListProps) {
+function OffersList({ offers, listType }: OffersListProps) {
   return (
-    <div className="cities__places-list places__list tabs__content">
+    <div className={`${listType === 'default' ? 'cities__places-list places__list tabs__content' : 'near-places__list places__list'}`}>
       {offers.map((offer) => (
         <Card key={offer.id} offer={offer} />
       ))}
